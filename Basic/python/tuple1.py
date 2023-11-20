05120d1f6d90506dac1a3c3af1df1b067f6be996
Maxwell Yingyou Li <31944577+superdba111@users.noreply.github.com>
2023-11-20 10:26:52 -0600

    Create tuple1.py

---
+++ Basic/python/tuple1.py
@@ -0,0 +1,10 @@
+person = ['tom', 5, 'male']
+
+name, age, gender = person
+# name='tom', age=5, gender='male'
+
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+first, second, *others = letters
+# first='A', second='B'
+# letters = ['C', 'D', 'E', 'F', 'G']
