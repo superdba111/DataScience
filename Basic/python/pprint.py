418ff39c6dfb2540074a7c403cfe0252301a415d
Maxwell Yingyou Li <31944577+superdba111@users.noreply.github.com>
2023-11-20 10:27:46 -0600

    Create pprint.py

---
+++ Basic/python/pprint.py
@@ -0,0 +1,9 @@
+x = [
+  {'apple': [1,2,3], 'orange':[4,5,6]},
+  {'pear': [7,8,9], 'pineapple':[10,11,12]},
+  {'durian':[13,14,15], 'banana':[16,17,18]}
+]
+
+print(x)
+from pprint import pprint
+pprint(x)
